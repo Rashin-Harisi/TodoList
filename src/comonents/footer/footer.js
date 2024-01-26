4f096ec9b8ef3c9767b4e51e905ee80fcfb396dd
Rashin Harisi <rashin.harisi@gmail.com>
2024-01-26 09:02:34 +0100

    Updating the Footer component

--- src/comonents/footer/footer.js
+++ src/comonents/footer/footer.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './footer.css';
+import { Button, ButtonGroup, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-const Footer = ()=>{
+const Footer = () => {
     return (
-        <div className='footer'></div>
+        <div className='footer'>
+            <Row style={{ width:'400px', marginTop:'10px'}}>
+                <Col>
+                    <div>1 item</div>
+                </Col>
+                <Col>
+                    <ButtonGroup aria-label="Basic example">
+                        <Button variant="outline-primary" size="sm">All</Button>
+                        <Button variant="outline-danger" size="sm">Active</Button>
+                        <Button variant="outline-success" size="sm">Completed</Button>
+                    </ButtonGroup>
+                </Col>
+            </Row>
+        </div>
     )
 };
 
